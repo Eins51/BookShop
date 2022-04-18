6cd6e44edfd1ee6fa6b6d7894c0b95287a25e404
bljcyyds <1186506721@qq.com>
2022-04-18 16:49:20 +0800

    Add OrderService class

---
+++ bookshop/src/service/OrderService.java
@@ -0,0 +1,52 @@
+package service;
+
+import dao.*;
+import model.*;
+import utils.*;
+
+import java.sql.*;
+import java.util.List;
+
+public class OrderService {
+    private OrderDao oDao = new OrderDao();
+    public void addOrder(Order order) {
+        Connection con = null;
+        try {
+            con = DBUtil.getConnection();
+            con.setAutoCommit(false);
+
+            oDao.insertOrder(con, order);
+            int id = oDao.getLastInsertId(con);
+            order.setId(id);
+            for(OrderItem item : order.getItemMap().values()) {
+                oDao.insertOrderItem(con, item);
+            }
+
+            con.commit();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            if(con!=null)
+                try {
+                    con.rollback();
+                } catch (SQLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+        }
+    }
+    public List<Order> selectAll(int userid){
+        List<Order> list=null;
+        try {
+            list = oDao.selectAll(userid);
+            for(Order o :list) {
+                List<OrderItem> l = oDao.selectAllItem(o.getId());
+                o.setItemList(l);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
