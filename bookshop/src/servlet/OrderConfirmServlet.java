0c5f5dc9c70c16b3fc13b36176f34406ea5ba298
bljcyyds <1186506721@qq.com>
2022-04-19 11:42:08 +0800

    Add OrderConfirmServlet class

---
+++ bookshop/src/servlet/OrderConfirmServlet.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import model.Order;
+import model.User;
+import org.apache.commons.beanutils.BeanUtils;
+import service.OrderService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+
+@WebServlet(name = "order_confirm",urlPatterns = "/order_confirm")
+public class OrderConfirmServlet extends HttpServlet {
+    private OrderService oService = new OrderService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Order o = (Order) request.getSession().getAttribute("order");
+        try {
+            BeanUtils.copyProperties(o, request.getParameterMap());
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        o.setDatetime(new Date());
+        o.setStatus(2);
+        o.setUser((User) request.getSession().getAttribute("user"));
+        oService.addOrder(o);
+        request.getSession().removeAttribute("order");
+
+        request.setAttribute("msg", "Order paid successfully！");
+        request.getRequestDispatcher("/order_success.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
