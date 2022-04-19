a4f61440700529cbfacd9917385e04f1432a21e3
bljcyyds <101611789+bljcyyds@users.noreply.github.com>
2022-04-19 11:53:11 +0800

    Update OrderSubmitServlet.java

--- bookshop/src/servlet/OrderSubmitServlet.java
+++ bookshop/src/servlet/OrderSubmitServlet.java
@@ -17,7 +17,21 @@ public class OrderSubmitServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if(request.getSession().getAttribute("user")!=null){
-            request.getRequestDispatcher("/order_submit.jsp").forward(request, response)
+            //request.getRequestDispatcher("/order_submit.jsp").forward(request, response)
+            Order o = (Order) request.getSession().getAttribute("order");
+            if(o!=null) {
+                if(o.getTotal()== 0){
+                    request.setAttribute("failMsg", "Please add books to shopping cart first！");
+                    request.getRequestDispatcher("/goods_cart.jsp").forward(request, response);
+                }
+                else {
+                    request.getRequestDispatcher("/order_submit.jsp").forward(request, response);
+                }
+            }
+            else {
+                request.setAttribute("failMsg", "Please add books to shopping cart first！");
+                request.getRequestDispatcher("/goods_cart.jsp").forward(request, response);
+            }
 
         }
         else {
