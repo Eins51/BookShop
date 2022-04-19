d2c8f526d44b9e04e78248b37febf3cd685a73e1
bljcyyds <1186506721@qq.com>
2022-04-19 11:46:38 +0800

    Add OrderSubmitServlet class

---
+++ bookshop/src/servlet/OrderSubmitServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import model.Order;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "order_submit",urlPatterns = "/order_submit")
+public class OrderSubmitServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("user")!=null){
+            request.getRequestDispatcher("/order_submit.jsp").forward(request, response)
+
+        }
+        else {
+            request.setAttribute("failMsg", "Please log in first！");
+            request.getRequestDispatcher("/user_login.jsp").forward(request, response);
+        }
+    }
+}
