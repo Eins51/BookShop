00c32ffbde1d133c9567aa66367bb0e985b59e7d
Eins51 <1115019759@qq.com>
2022-04-21 01:34:33 +0800

    Upload GoodsListServlet

--- bookshop/src/servlet/GoodsListServlet.java
+++ bookshop/src/servlet/GoodsListServlet.java
@@ -1,6 +1,5 @@
 package servlet;
 
-import model.Goods;
 import model.Page;
 import model.Type;
 import service.GoodsService;
@@ -12,26 +11,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "goods_List",urlPatterns = "/goods_list")
 public class GoodsListServlet extends HttpServlet {
-    private GoodsService gService=new GoodsService();
-    private TypeService tService=new TypeService();
+    private GoodsService gService = new GoodsService();
+    private TypeService tService = new TypeService();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id=0;
-        if(request.getParameter("typeid")!=null)
+        int id = 0;
+        if(request.getParameter("typeid") != null)
         {
-            id=Integer.parseInt(request.getParameter("typeid"));
+            id = Integer.parseInt(request.getParameter("typeid"));
         }
-        int pageNumber=1;
-        if(request.getParameter("pageNumber")!=null) {
+        int pageNumber = 1;
+        if(request.getParameter("pageNumber") != null) {
             try {
-                pageNumber=Integer.parseInt(request.getParameter("pageNumber"));
+                pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
             }
             catch (Exception e)
             {
@@ -39,27 +37,25 @@ public class GoodsListServlet extends HttpServlet {
             }
 
         }
-        Type t=null;
-        if(id!=0)
+        Type t = null;
+        if(id != 0)
         {
-            t=tService.selectTypeNameByID(id);
+            t = tService.selectTypeNameByID(id);
         }
-        request.setAttribute("t",t);
-        //List<Goods> list=gService.selectGoodsByTypeID(id,1,8);
-        //request.setAttribute("goodsList",list);
-        if(pageNumber<=0)
-            pageNumber=1;
-        Page p=gService.selectPageByTypeID(id,pageNumber);
+        request.setAttribute("t", t);
+        if(pageNumber <= 0)
+            pageNumber = 1;
+        Page p=gService.selectPageByTypeID(id, pageNumber);
 
-        if(p.getTotalPage()==0)
+        if(p.getTotalPage() == 0)
         {
             p.setTotalPage(1);
             p.setPageNumber(1);
         }
         else {
-            if(pageNumber>=p.getTotalPage()+1)
+            if(pageNumber >= p.getTotalPage()+1)
             {
-                p=gService.selectPageByTypeID(id,p.getTotalPage());
+                p = gService.selectPageByTypeID(id,p.getTotalPage());
             }
         }
 
