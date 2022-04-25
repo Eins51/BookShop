dc5ffda37a8319da55095ce0774ae82b15bb934d
zxxcosw <103319557+zxxcosw@users.noreply.github.com>
2022-04-25 14:22:51 +0800

    Add files via upload

---
+++ bookshop/src/servlet/OrderStatusChangeServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import service.OrderService;
+import model.Order;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "status_update",urlPatterns = "/status_update")
+public class OrderStatusChangeServlet extends HttpServlet {
+    private OrderService oService = new OrderService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        int status = Integer.parseInt(request.getParameter("status"));
+        if(status==5){
+            oService.statusReturn(id);
+        }
+        else if(status==6){
+            oService.statusCancel(id);
+        }
+        response.sendRedirect("./order_list?status="+status);
+    }
+}
