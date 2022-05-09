c64f4d86f81a3c1ef7e0a4255613a3c4171f0a6c
bljcyyds <101611789+bljcyyds@users.noreply.github.com>
2022-05-09 20:30:39 +0800

    Add files via upload

---
+++ bookshop/src/servlet/AdminTypeDeleteServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import service.TypeService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "admin_type_delete",urlPatterns = "/admin/type_delete")
+public class AdminTypeDeleteServlet extends HttpServlet {
+    private TypeService tService = new TypeService();
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        boolean isSuccess = tService.delete(id);
+        if(isSuccess) {
+            request.setAttribute("msg", "Delete successfully!");
+        }else {
+            request.setAttribute("failMsg", "There are still some books in this type. Fail to delete！");
+        }
+        request.getRequestDispatcher("/admin/type_list").forward(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+}
