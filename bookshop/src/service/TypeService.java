04eb69950087a9cab11f654031625ecc4b7d0858
bljcyyds <101611789+bljcyyds@users.noreply.github.com>
2022-05-09 20:42:32 +0800

    Update TypeService.java

--- bookshop/src/service/TypeService.java
+++ bookshop/src/service/TypeService.java
@@ -28,5 +28,41 @@ public class TypeService {
         }
         return type;
     }
+    public Type select(int id) {
+        Type t=null;
+        try {
+            t = tDao.select(id);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return t;
+    }
+    public void insert(Type t) {
+        try {
+            tDao.insert(t);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public void update(Type t) {
+        try {
+            tDao.update(t);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public boolean delete(int id) {
+        try {
+            tDao.delete(id);
+            return true;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
