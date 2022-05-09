c83442cbfa0e1e116cb7ef72640309a20c93a146
bljcyyds <101611789+bljcyyds@users.noreply.github.com>
2022-05-09 20:45:12 +0800

    Update TypeDao.java

--- bookshop/src/dao/TypeDao.java
+++ bookshop/src/dao/TypeDao.java
@@ -21,4 +21,24 @@ public class TypeDao
         String sql="select * from type where id=?";
         return r.query(sql,new BeanHandler<Type>(Type.class),typeid);
     }
+       public Type select(int id) throws SQLException {
+        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
+        String sql = "select * from type where id = ?";
+        return r.query(sql, new BeanHandler<Type>(Type.class),id);
+    }
+    public void insert(Type t) throws SQLException {
+        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
+        String sql = "insert into type(name) values(?)";
+        r.update(sql,t.getName());
+    }
+    public void update(Type t) throws SQLException {
+        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
+        String sql = "update type set name=? where id = ?";
+        r.update(sql,t.getName(),t.getId());
+    }
+    public void delete(int id) throws SQLException {
+        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
+        String sql = "delete from type where id = ?";
+        r.update(sql,id);
+    }
 }
